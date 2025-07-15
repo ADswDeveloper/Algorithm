50488bbe463c5a08938844ced103e3643bf3101d
ziano <jhjeong_466@dgu.ac.kr>
2025-07-15 23:48:10 +0900

    formula method solved

--- TBCPP_02_Floyd's_Triangle/TBCPP_02_Floyd's_Triangle_Formula.cpp
+++ TBCPP_02_Floyd's_Triangle/TBCPP_02_Floyd's_Triangle_Formula.cpp
@@ -1,8 +1,24 @@
 #include <iostream>
 using namespace std;
 
-int formula()
+int main()
 {
+    int num = 0;
+    while (true)
+    {
+        cout << " 숫자를 입력하세요 : ";
+        cin >> num;
 
+        for (int i = 1; i <= num; i++)
+        {
+            for (int j = i * (i + 1) / 2 - (i - 1); j <= i * (i + 1) / 2; j++)
+            {
+                cout << j << " ";
+            }
+            cout << endl; 
+        }
+    }
+
+    // 어떤 수 n 에서 어떤 수 x까지 i 개 만큼 있다 -> x는 n - i + 1
     return 0;
 }

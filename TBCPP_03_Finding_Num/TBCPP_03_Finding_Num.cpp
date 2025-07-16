f1584597bb148f197ea65d2a07a6c057022007c4
ziano <jhjeong_466@dgu.ac.kr>
2025-07-16 23:52:36 +0900

    Numer_guessing 추가

---
+++ TBCPP_03_Finding_Num/TBCPP_03_Finding_Num.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <random>
+
+using namespace std;
+
+int main()
+{
+    // 난수 생성
+    // 
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> distrib(1, 99);
+
+    int number = distrib(gen);
+    while (1)
+    {
+        int guess = 0;
+        cout << "입력 :";
+        cin >> guess;
+
+        if (guess == number)
+        {
+            cout << "정답입니다!" << endl;
+            break;
+        }
+        else if(guess > number)
+        {
+            cout << "너무 커요!" << endl;
+        }
+        else
+        {
+            cout << "너무 작아요!" << endl;
+        }
+    }
+}

f9736699cb2eb4c26e29c9f48745f1705093a34e
jinho2.jeong <jinho2.jeong@hlcompany.com>
2025-08-08 18:09:34 +0900

    sim

---
+++ BKDG_11_Simulation/BKDG_11_Simulation.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int N, M;
+int room[10][10];
+
+
+int main()
+{
+    cin >> N >> M;
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < M; j++)
+        {
+            cin >> room[i][j];
+        }
+    }
+    
+    
+}
